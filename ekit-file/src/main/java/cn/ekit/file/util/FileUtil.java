9af0536d88d73bf4ced9f297b6ea21c59babe8c3
wangcheng <1026878898@qq.com>
2022-05-21 16:57:27 +0800

    init project

---
+++ ekit-file/src/main/java/cn/ekit/file/util/FileUtil.java
@@ -0,0 +1,43 @@
+package cn.ekit.file.util;
+
+
+import cn.ekit.io.util.IoUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author cheng
+ * @date 2022/5/21 15:18
+ * description
+ */
+public class FileUtil {
+
+    /**
+     * 读取文件内容
+     *
+     * @param file 文件全路径
+     * @return 文件内容
+     */
+    public static String readFile(String file) throws IOException {
+        if (StringUtils.isBlank(file)) {
+            return null;
+        }
+        FileInputStream fileInputStream;
+        fileInputStream = new FileInputStream(file);
+        ByteArrayOutputStream out;
+        out = new ByteArrayOutputStream(10240);
+        byte[] buff = new byte[2048];
+        int len = 0;
+        while ((len = fileInputStream.read(buff, 0, buff.length)) != -1) {
+            out.write(buff, 0, len);
+        }
+        out.flush();
+        String ret = new String(out.toByteArray());
+        IoUtil.closeQuietly(out);
+        IoUtil.closeQuietly(fileInputStream);
+        return ret;
+    }
+}
---
+++ ekit-file/src/main/java/cn/ekit/io/util/IoUtil.java
@@ -0,0 +1,23 @@
+package cn.ekit.io.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author cheng
+ * @date 2022/5/21 15:48
+ * description
+ */
+public class IoUtil {
+
+    public static void closeQuietly(Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
